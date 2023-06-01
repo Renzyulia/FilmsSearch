e3881cfa40cb81d32eb7ca88483fe4d03a40e75c
Julia <renzyulia@mail.ru>
2023-06-01 14:22:13 +0300

    Created the interface of the view for showing a list of films

--- ReshenieTestApp/FilmsView.swift
+++ ReshenieTestApp/FilmsView.swift
@@ -5,4 +5,107 @@
 //  Created by Yulia Ignateva on 01.06.2023.
 //
 
-import Foundation
+import UIKit
+
+final class FilmsView: UIView {
+    private let tableView = UITableView()
+    
+    private let tableViewDelegate: UITableViewDelegate
+    private let tableViewDataSource: UITableViewDataSource
+    
+    init(tableViewDelegate: UITableViewDelegate, tableViewDataSource: UITableViewDataSource) {
+        self.tableViewDelegate = tableViewDelegate
+        self.tableViewDataSource = tableViewDataSource
+        super.init(frame: .zero)
+        
+        configureTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = tableViewDelegate
+        tableView.dataSource = tableViewDataSource
+        
+        addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
+
+struct Genre {
+    let name: String
+    let year: Int
+}
+
+final class FilmsCell: UITableViewCell {
+    private let iconView = UIImageView()
+    private let titleLabel = UILabel()
+    private let genreLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureIconView()
+        configureTitleLabel()
+        configureGenreLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(title: String, genre: Genre, icon: UIImage) {
+        iconView.image = icon
+        titleLabel.text = title
+        genreLabel.text = "\(genre.name)" + " " + "(\(genre.year)"
+    }
+    
+    private func configureIconView() {
+        addSubview(iconView)
+        
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            iconView.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+            iconView.leftAnchor.constraint(equalTo: leftAnchor, constant: 15),
+            iconView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15)
+        ])
+    }
+    
+    private func configureTitleLabel() {
+        titleLabel.font = UIFont.specialFont(size: 16, style: .medium)
+        titleLabel.textColor = .black
+        titleLabel.numberOfLines = 1
+        
+        addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 26),
+            titleLabel.leftAnchor.constraint(equalTo: iconView.rightAnchor, constant: 15),
+            titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -50)
+        ])
+    }
+    
+    private func configureGenreLabel() {
+        genreLabel.font = UIFont.specialFont(size: 14, style: .medium)
+        genreLabel.textColor = .gray
+        genreLabel.numberOfLines = 1
+        
+        addSubview(genreLabel)
+        
+        genreLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            genreLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            genreLabel.leftAnchor.constraint(equalTo: iconView.rightAnchor, constant: 15),
+            genreLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -27)
+        ])
+    }
+}
--- ReshenieTestApp/FilmsViewController.swift
+++ ReshenieTestApp/FilmsViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FilmsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    private func configureNavigationBar() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        // MARK: - configure title
+        let titleLabel = UILabel()
+        let title = NSAttributedString(
+            string: "Фильмы",
+            attributes: [
+                .font: UIFont.specialFont(size: 25, style: .medium),
+                .foregroundColor: UIColor.black
+            ]
+        )
+        
+        titleLabel.attributedText = title
+        titleLabel.textAlignment = .left
+        
+        navigationBar.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leftAnchor.constraint(equalTo: navigationBar.leftAnchor, constant: 16),
+            titleLabel.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -5)
+        ])
+        
+        // MARK: - configure searchButton
+        let searchButton = UIBarButtonItem(image: UIImage(named: "SearchIcon"), style: .plain, target: self, action: #selector(didTapSearchButton))
+        
+        navigationItem.rightBarButtonItem = searchButton
+    }
+    
+    @objc private func didTapSearchButton() {
+        print("Search")
+    }
 }
 
